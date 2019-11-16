755e3bc730b2c620905895f46e4b32018488adcd
Connor D <daliposc@oregonstate.edu>
2019-11-16 04:25:12 -0800

    Test importing scripts I wrote

--- if_main_testing.py
+++ if_main_testing.py
@@ -1,6 +1,7 @@
 import get_wash_co_site_info
 import pandas as pd
 
+##Call a function from an imported python file that I made
 df = pd.read_csv("data/taxlots_hillsboro_sql_table_export.csv")
 tlid_df = get_wash_co_site_info.get_webpage("1N228AA00400")
 print(tlid_df)
